6a822b12c985697fbe4c936134c409c759b33270
Jason Kelsey-De France <jason@may54th.com>
2017-09-20 18:04:59 -0700

    Remove outdated comment

--- pollster/regex.go
+++ pollster/regex.go
@@ -11,7 +11,6 @@ func init() {
 	containerInstanceIdFromArnRegEx = regexp.MustCompile(`arn:aws:ecs:[a-z0-9-]*:\d*:container-instance\/([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})`)
 }
 
-// TODO: TESTS/BENCHING
 func parseContainerInstanceId(containerInstanceArn string) (string, error) {
 	matches := containerInstanceIdFromArnRegEx.FindStringSubmatch(containerInstanceArn)
 
